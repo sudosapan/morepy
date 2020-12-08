47d5852b7f4458d8874577679168d64af36289e0
Sapan Vashishth <sudosapan@localhost.localdomain>
2020-12-08 13:27:04 +0530

    Initial

---
+++ hello.py
@@ -0,0 +1,15 @@
+print ("Hello, World!!")
+
+if True:
+    print ("True")
+else:
+    print ("False")
+
+def hello(name):
+    """
+    Prints name with hello
+    """
+    print("Hello "+ name)
+
+
+hello("Sapan")
